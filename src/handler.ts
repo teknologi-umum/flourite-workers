6388be1f58864410748d92b55e4c78430823457d
Reinaldy Rafli <aldy505@tutanota.com>
2021-11-21 21:36:31 +0700

    feat: initialize

---
+++ src/handler.ts
@@ -0,0 +1,44 @@
+import flourite from 'flourite'
+
+export async function handleRequest(request: Request): Promise<Response> {
+  const accept = request.headers.get('Accept') || 'text/plain'
+  const body = await request.text()
+  if (body.length === 0) {
+    return new Response('Body is empty', {
+      status: 400,
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+    })
+  }
+  const query = new URL(request.url).searchParams
+  const shiki = Boolean(query.get('shiki')) || false
+  const statistics = Boolean(query.get('statistics')) || false
+
+  const result = flourite(body, { shiki })
+
+  const resStatistics = statistics ? { statistics: result.statistics } : {}
+
+  switch (accept) {
+    case 'application/json':
+      return new Response(
+        JSON.stringify({
+          language: result.language,
+          ...resStatistics,
+        }),
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      )
+    case 'text/html':
+    case 'text/plain':
+    default:
+      return new Response(result.language, {
+        headers: {
+          'Content-Type': 'text/html',
+        },
+      })
+  }
+}
---
+++ test/handler.test.ts
@@ -0,0 +1,98 @@
+import { handleRequest } from '../src/handler'
+import makeServiceWorkerEnv from 'service-worker-mock'
+
+declare var global: any
+
+describe('handle', () => {
+  beforeEach(() => {
+    Object.assign(global, makeServiceWorkerEnv())
+    jest.resetModules()
+  })
+
+  test('handle simple POST request', async () => {
+    const result = await handleRequest(
+      new Request('/', { method: 'POST', body: 'console.log("Hello world");' }),
+    )
+    expect(result.status).toEqual(200)
+    const text = await result.text()
+    expect(text).toEqual('Javascript')
+  })
+
+  test('handle POST request with params shiki', async () => {
+    const result = await handleRequest(
+      new Request('/?shiki=true', {
+        method: 'POST',
+        body: 'using System;\nConsole.WriteLine("Hello World");',
+      }),
+    )
+    expect(result.status).toEqual(200)
+    const text = await result.text()
+    expect(text).toEqual('csharp')
+  })
+
+  test('handle JSON request', async () => {
+    const result = await handleRequest(
+      new Request('/', {
+        method: 'POST',
+        body: 'func main() {\nfmt.Println("Hello World")\n}',
+        headers: { Accept: 'application/json' },
+      }),
+    )
+    expect(result.status).toEqual(200)
+    const text = await result.json()
+    expect(text).toEqual({ language: 'Go' })
+  })
+
+  test('handle statistics', async () => {
+    const result = await handleRequest(
+      new Request('/?statistics=true', {
+        method: 'POST',
+        body: 'func main() {\nfmt.Println("Hello World")\n}',
+        headers: { Accept: 'application/json' },
+      }),
+    )
+    expect(result.status).toEqual(200)
+    const text = await result.json()
+    expect(text).toEqual({
+      language: 'Go',
+      statistics: {
+        C: 0,
+        'C#': 0,
+        'C++': 0,
+        CSS: 0,
+        Clojure: 0,
+        Dockerfile: 0,
+        Elixir: 0,
+        Go: 10,
+        HTML: 0,
+        JSON: 0,
+        Java: 0,
+        Javascript: -20,
+        Julia: 0,
+        Kotlin: 0,
+        Lua: 0,
+        Markdown: 0,
+        PHP: 0,
+        Pascal: 0,
+        Python: 0,
+        Ruby: 0,
+        Rust: -20,
+        SQL: 0,
+        Unknown: 1,
+        YAML: 0,
+      },
+    })
+  })
+
+  test('handle empty body', async () => {
+    const result = await handleRequest(
+      new Request('/', {
+        method: 'POST',
+        body: '',
+      }),
+    )
+    expect(result.status).toEqual(200)
+    const text = await result.text()
+    expect(text).toEqual('Body is empty')
+  })
+})
